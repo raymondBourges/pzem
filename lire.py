ee8ae07d672912cc8b4bd3857a5bb8b024a18c13
Raymond Bourges <raymond.bourges@pc-scol.fr>
2023-10-26 18:57:46 +0200

    lire renvoie les même infos que pzem_004t

--- lire.py
+++ lire.py
@@ -11,14 +11,29 @@ client = ModbusSerialClient(method="rtu", port=serial_port, baudrate=9600, stopb
 client.connect()
 
 try:
-    response = client.read_input_registers(0, 10, slave=1)
-    if response:
-        # La réponse contient les valeurs lues
-        print("Valeurs lues : " + str(response.registers))
+    data = client.read_input_registers(0, 10, slave=1)
+    if data:
+        print("Valeurs lues : " + str(data.registers))
+        voltage = data.getRegister(0) / 10.0 # [V]
+        current = (data.getRegister(1) + (data.getRegister(2) << 16)) / 1000.0 # [A]
+        power = (data.getRegister(3) + (data.getRegister(4) << 16)) / 10.0 # [W]
+        energy = data.getRegister(5) + (data.getRegister(6) << 16) # [Wh]
+        frequency = data.getRegister(7) / 10.0 # [Hz]
+        powerFactor = data.getRegister(8) / 100.0
+        alarm = data.getRegister(9) # 0 = no alarm# La réponse contient les valeurs lues
+        print('Voltage [V]: ', voltage)
+        print('Current [A]: ', current)
+        print('Power [W]: ', power) # active power (V * I * power factor)
+        print('Energy [Wh]: ', energy)
+        print('Frequency [Hz]: ', frequency)
+        print('Power factor []: ', powerFactor)
+        print('Alarm : ', alarm)
     else:
         print("Aucune réponse.")
 except Exception as e:
     print("Erreur : " + str(e))
+
 # Assurez-vous de fermer la connexion après avoir terminé
 client.close()
                 
+print("************************")                
